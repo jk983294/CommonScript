8d9cf2ce5b29036fe2e46e6bf682bebcc27fe81a
jk983294 <jk983294@gmail.com>
2015-11-11 12:55:03 +0800

    Update ddl.sql

---
+++ db/db2/ddl.sql
@@ -0,0 +1,24 @@
+CREATE TABLE "schema"."table1" (
+	"a" VARCHAR(20) NOT NULL,
+	"b" VARCHAR(50) NOT NULL,
+	"c" INTEGER,
+	"d" VARCHAR(20)
+	"e" VARCHAR(20)
+	"f" TIMESTAMP ,
+	"g" VARCHAR(20)
+)
+in PROC_BASE
+INDEX in PROC_BASE_IX;
+
+ALTER TABLE "schema"."table1" ADD CONSTRAINT "table1_PRIMARY_KEY" PRIMARY KEY ("a","b");
+
+drop table "schema"."table1";
+
+-- export, replace 'null' with null
+SELECT 'INSERT INTO schema.table1 VALUES ('''
+	||a||''', '''                    
+	||b||''', '  
+	||c||', '''  
+	||coalesce(d, 'null')||''', '''  
+	||coalesce(e, 'null')||''', current_timestamp, ''prod'' );'                
+FROM schema.table1;
