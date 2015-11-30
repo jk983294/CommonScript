727bb527a156b4ad13b71974159bb99b3452734a
jk983294 <jk983294@gmail.com>
2015-11-30 17:11:24 +0800

    Update ddl.sql

--- db/db2/ddl.sql
+++ db/db2/ddl.sql
@@ -1,4 +1,4 @@
-CREATE TABLE "schema"."table1" (
+CREATE TABLE schema.table1 (
 	"a" VARCHAR(20) NOT NULL,
 	"b" VARCHAR(50) NOT NULL,
 	"c" INTEGER,
@@ -12,9 +12,15 @@ INDEX in PROC_BASE_IX;
 
 CREATE TABLE schema.table1_temp LIKE schema.table1;
 
-ALTER TABLE "schema"."table1" ADD CONSTRAINT "table1_PRIMARY_KEY" PRIMARY KEY ("a","b");
+-- drop
+drop table schema.table1;
+drop PROCEDURE schema.sp_table1_something;
+ALTER TABLE schema.table1 DROP PRIMARY KEY;
+ALTER TABLE schema.table1 DROP COLUMN column1;
 
-drop table "schema"."table1";
+-- alter 
+ALTER TABLE schema.table1 RENAME COLUMN UPDATED_BY TO CREATED_BY;
+ALTER TABLE schema.table1 ADD CONSTRAINT "table1_PRIMARY_KEY" PRIMARY KEY ("a","b");
 
 -- export, replace 'null' with null
 SELECT 'INSERT INTO schema.table1 VALUES ('''
