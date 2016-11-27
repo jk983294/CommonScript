139ac508cdd34256f4273ca3162585b589d712c5
jk983294 <jk983294@gmail.com>
2016-11-27 19:49:10 +0800

    add batch

--- cpp/boost/string/string_algorithm.cpp
+++ cpp/boost/string/string_algorithm.cpp
@@ -1,12 +1,37 @@
 #include <iostream>
 #include <string>
+#include <vector>
 #include <boost/algorithm/string.hpp>
 #include <boost/algorithm/string/trim_all.hpp>
 using namespace std;
 using namespace boost;
 
+bool is_executable(string&& filename)
+{
+    return iends_with(filename, ".exe");
+}
+
+bool is_all_lower(string&& text)
+{
+    return all(text, is_lower());
+}
+
+void split_demo()
+{
+    string str1("hello abc-*-ABC-*-aBc goodbye");
+
+    typedef vector< string > split_vector_type;
+    split_vector_type SplitVec;
+    split(SplitVec, str1, is_any_of("-*-"), token_compress_on);      // SplitVec == { "hello abc","ABC","aBc goodbye" }
+    for (auto& s : SplitVec)
+    {
+        cout << s << endl;
+    }
+}
+
 int main(int argc, char *argv[])
 {
+    cout << boolalpha;
     string test = "\nhello   world \r\n";
     trim(test);
     cout << "<" << test << ">" << endl;
@@ -14,6 +39,11 @@ int main(int argc, char *argv[])
     cout << "<" << test << ">" << endl;
     to_upper(test);
     cout << "<" << test << ">" << endl;
+
+    cout << is_executable("a.exe") << endl;
+    cout << is_all_lower("abexe") << endl;
+
+    split_demo();
 
     getchar();
     return 0;
