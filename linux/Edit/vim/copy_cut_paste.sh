50e2e6ed364687892c449e10e5395f04c52bc511
jk983294 <jk983294@gmail.com>
2017-03-16 23:14:03 +0800

    encoding

--- linux/Edit/vim/copy_cut_paste.sh
+++ linux/Edit/vim/copy_cut_paste.sh
@@ -7,7 +7,7 @@ p                           paste(put) the yanked text after the cursor.
 P                           paste(put) the yanked text before the cursor.
 shift + insert              in edit mode, copy
 
-# Before you paste, type this in normal mode:
+# Before you paste, to disable auto indent temporarily, type this in normal mode:
 :set paste
 # Then enter insert mode. You will see the status bar say insert (paste). Paste your code. Hit ESC to return to normal mode, and:
 :set nopaste
