2924463715bd293ca3d942bf131fb3196d535e73
jk983294 <jk983294@gmail.com>
2017-06-27 22:16:09 +0800

    add py

--- python/structure/string_example.py
+++ python/structure/string_example.py
@@ -2,12 +2,20 @@
 
 # string
 str1 = 'Hello World!'
-print str1                                                      # Prints complete string
-print str1[0]                                                   # Prints first character of the string
-print str1[2:5]                                                 # Prints characters starting from 3rd to 5th
-print str1[2:]                                                  # Prints string starting from 3rd character
-print str1 * 2                                                  # Prints string two times
-print str1 + "TEST"                                             # Prints concatenated string
+# Prints complete string
+print str1
+# Prints first character of the string
+print str1[0]
+# Prints characters starting from 3rd to 5th
+print str1[2:5]
+# Prints string starting from 3rd character
+print str1[2:]
+# remove first and last character
+print str1[1:-1]
+# Prints string two times
+print str1 * 2
+# Prints concatenated string
+print str1 + "TEST"
 print 'H' in str1
 
 # raw string
@@ -29,28 +37,41 @@ print min('hello, world!')                                      # ' '
 print 'hello, world!'.count('ll')                               # 1
 
 print 'hello, world!'.find('ll')                                # 2
-print 'hello, world!'.index('ll')                               # 2 raises an exception if no such index exists
+# 2 raises an exception if no such index exists
+print 'hello, world!'.index('ll')
 print 'hello, world!'.rfind('o')                                # 8
-print 'hello, world!'.rindex('o')                               # 8 raises an exception if no such index exists
+# 8 raises an exception if no such index exists
+print 'hello, world!'.rindex('o')
 
-print 'hello, world!'.center(20, '-')                           # '---hello, world!----'
-print 'hello, world!'.zfill(20)                                 # '0000000hello, world!'
-print 'hello, \tworld!'.expandtabs(4)                           # 'hello,  world!'
+# '---hello, world!----'
+print 'hello, world!'.center(20, '-')
+# '0000000hello, world!'
+print 'hello, world!'.zfill(20)
+# 'hello,  world!'
+print 'hello, \tworld!'.expandtabs(4)
 print 'hello, WORLD!'.swapcase()                                # 'HELLO, world!'
-print ' hello, world! '.rjust(20, '-')                          # '----- hello, world!'
-print ' hello, world! '.ljust(20, '-')                          # ' hello, world! -----'
+# '----- hello, world!'
+print ' hello, world! '.rjust(20, '-')
+# ' hello, world! -----'
+print ' hello, world! '.ljust(20, '-')
 print '88example88'.strip('8')                                  # 'example'
 print '88example88'.rstrip('8')                                 # '88example'
 print '88example88'.lstrip('8')                                 # 'example88'
 
 print '-'.join(("a", "b", "c"))                                 # 'a-b-c'
-print 'hello, world!'.replace("hello", "HELLO")                 # 'HELLO, world!'
-print 'hello, \nworld!'.split()                                 # ['hello,', 'world!']
-print 'hello, \nworld!'.split(" ")                              # ['hello,', '\nworld!']
-print 'hello, \nworld!'.splitlines()                            # ['hello, ', 'world!']
+# 'HELLO, world!'
+print 'hello, world!'.replace("hello", "HELLO")
+# ['hello,', 'world!']
+print 'hello, \nworld!'.split()
+# ['hello,', '\nworld!']
+print 'hello, \nworld!'.split(" ")
+# ['hello, ', 'world!']
+print 'hello, \nworld!'.splitlines()
 
-print 'hello, world!'.decode(encoding='UTF-8', errors='strict')     # decodes the string using the codec registered for encoding
-print 'hello, world!'.encode(encoding='UTF-8', errors='strict')     # an encoded version of the string
+# decodes the string using the codec registered for encoding
+print 'hello, world!'.decode(encoding='UTF-8', errors='strict')
+# an encoded version of the string
+print 'hello, world!'.encode(encoding='UTF-8', errors='strict')
 
 print 'ABC'.lower()
 print 'hello, world!'.islower()
